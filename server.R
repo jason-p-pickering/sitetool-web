c0d22f0aac4121333573d5e9fcba50245769bd9e
Jason Pickering <jason.p.pickering@gmail.com>
2019-03-15 14:12:16 +0200

    Initial commit.

---
+++ server.R
@@ -0,0 +1,219 @@
+library(shiny)
+library(shinyjs)
+require(magrittr)
+require(purrr)
+require(dplyr)
+require(datimvalidation)
+require(ggplot2)
+
+
+source("./utils.R")
+
+shinyServer(function(input, output, session) {
+  
+  ready <- reactiveValues(ok = FALSE)
+  
+  observeEvent(input$file1, {
+    shinyjs::enable("validate")
+    ready$ok <- FALSE
+  }) 
+  
+  observeEvent(input$validate, {
+    shinyjs::disable("validate")
+    ready$ok <- TRUE
+  })  
+  
+  
+  output$ui <- renderUI({
+    
+    if (user_input$authenticated == FALSE) {
+      ##### UI code for login page
+      fluidPage(
+        fluidRow(
+          column(width = 2, offset = 5,
+                 br(), br(), br(), br(),
+                 uiOutput("uiLogin"),
+                 uiOutput("pass")
+          )
+        )
+      )
+    } else {
+      fluidPage(
+        tags$head(tags$style(".shiny-notification {
+                             position: fixed;
+                             top: 10%;
+                             left: 33%;
+                             right: 33%;}")),
+        sidebarLayout(
+          sidebarPanel(
+            shinyjs::useShinyjs(),
+            fileInput(
+              "file1",
+              "Choose SiteTool (Must be XLSX!):",
+              accept = c(
+                "application/xlsx",
+                ".xlsx"
+              )
+            ),
+            tags$hr(),
+            actionButton("validate","Validate"),
+            tags$hr(),
+            downloadButton("downloadFlatPack", "Download FlatPacked SiteTool")
+          ),
+          mainPanel(tabsetPanel(
+            type = "tabs",
+            tabPanel("Messages",   tags$ul(uiOutput('messages'))),
+            tabPanel("Indicator summary", dataTableOutput("indicator_summary")),
+            tabPanel("Validation rules", dataTableOutput("vr_rules"))
+          ))
+        ))
+  }
+})
+  
+  
+  user_input <- reactiveValues(authenticated = FALSE, status = "")
+  
+  observeEvent(input$login_button, {
+    is_logged_in<-FALSE
+    user_input$authenticated <-DHISLogin(input$server,input$user_name,input$password)
+  })   
+  
+  # password entry UI componenets:
+  #   username and password text fields, login button
+  output$uiLogin <- renderUI({
+    
+    wellPanel(fluidRow(
+      img(src='pepfar.png', align = "center"),
+      h4("Welcome to the SiteTool Validation App. Please login with your DATIM credentials:")
+    ),
+    fluidRow(
+      textInput("user_name", "Username: ",width = "600px"),
+      passwordInput("password", "Password:",width = "600px"),
+      actionButton("login_button", "Log in!")
+    ))
+  })
+  
+  
+  validate<-function() {
+    
+    shinyjs::hide("downloadFlatPack")
+    
+    if (!ready$ok) {return(NULL)}
+  
+    inFile <- input$file1
+    messages<-""
+    
+    if ( is.null( inFile ) ) return( NULL )
+    
+    messages<-list()
+    
+    withProgress(message = 'Validating file', value = 0,{
+      
+      incProgress(0.1, detail = ("Validating your SiteTool"))
+      d<-tryCatch({
+        datapackr::unPackSiteToolData(inFile$datapath)},
+        error = function(e){
+          return(e)
+        })
+      
+      if (!inherits(d,"error") & !is.null(d)) {
+        
+        incProgress(0.1, detail = ("Checking validation rules"))
+        d <- validateSiteData(d)
+        incProgress(0.1, detail = ("Producing download format"))
+        d %<>% adornSiteData()
+        
+        shinyjs::show("downloadFlatPack")
+      }
+    })
+    return(d)
+    
+  }
+  
+  
+  validation_results <- reactive({ validate() })
+  
+  
+  output$indicator_summary<-renderDataTable({
+    
+    vr<-validation_results()
+    
+    if (!inherits(vr,"error") & !is.null(vr)){
+      vr  %>% 
+        purrr::pluck(.,"datim") %>%
+        purrr::pluck(.,"site_data_pretty") %>%
+        dplyr::select(dataElement,agency,value) %>% 
+        dplyr::group_by(dataElement,agency) %>% 
+        dplyr::summarise(value = format( sum(value) ,big.mark=',', scientific=FALSE)) %>%
+        dplyr::arrange(dataElement,agency) 
+      
+    } else {
+      NULL
+    }
+  })
+  
+
+  output$vr_rules <- renderDataTable({ 
+    
+    vr<-validation_results()
+    
+    if (!inherits(vr,"error")  & !is.null(vr)){
+      
+      vr %>%
+        purrr::pluck(.,"datim") %>%
+        purrr::pluck(.,"vr_rules_check")  
+      
+      } else {
+          NULL
+        }
+    
+  })
+  
+  output$downloadFlatPack <- downloadHandler(
+    
+    filename = function() {
+      
+      prefix <- "site_tool_flatpack"
+    
+      date<-format(Sys.time(),"%Y%m%d_%H%M%S")
+      
+      paste0(paste(prefix,date,sep="_"),".xlsx")
+    },
+    content = function(file) {
+      
+      download_data <- validation_results() %>% 
+        purrr::pluck(.,"datim")
+      
+      openxlsx::write.xlsx(download_data, file = file)
+      
+    })
+    
+  
+  output$messages <- renderUI({
+    
+    vr<-validation_results()
+    messages<-NULL
+    
+    if ( is.null(vr)) {
+      return(NULL)
+    }
+    
+    if ( inherits(vr,"error") ) {
+      return( paste0("ERROR! ",vr$message) )
+      
+    } else {
+      
+      messages <- validation_results() %>%
+        purrr::pluck(., "info") %>%
+        purrr::pluck(., "warningMsg")
+      
+      if (!is.null(messages))  {
+        lapply(messages, function(x)
+          tags$li(x))
+      } else
+      {
+        tags$li("No Issues with Integrity Checks: Congratulations!")
+      }
+    }
+  })
+})
---
+++ utils.R
@@ -0,0 +1,151 @@
+require(datapackr)
+require(scales)
+options(shiny.maxRequestSize = 100 * 1024 ^ 2)
+options("baseurl" = "http://127.0.0.1:8080/")
+
+DHISLogin <- function(baseurl, username, password) {
+  httr::set_config(httr::config(http_version = 0))
+  url <- URLencode(URL = paste0(getOption("baseurl"), "api/me"))
+  #Logging in here will give us a cookie to reuse
+  r <- httr::GET(url,
+                 httr::authenticate(username, password),
+                 httr::timeout(60))
+  if (r$status != 200L) {
+    return(FALSE)
+  } else {
+    me <- jsonlite::fromJSON(httr::content(r, as = "text"))
+    options("organisationUnit" = me$organisationUnits$id)
+    return(TRUE)
+  }
+}
+
+validateSiteData <- function(d) {
+  #Validation rule checking
+  vr_data <- d$datim$site_data
+  names(vr_data) <- c("dataElement",
+                      "period",
+                      "orgUnit",
+                      "categoryOptionCombo",
+                      "attributeOptionCombo",
+                      "value")
+  vr_data$attributeOptionCombo <-
+    datimvalidation::remapMechs(vr_data$attributeOptionCombo,
+                                getOption("organisationUnit"),
+                                "code",
+                                "id")
+  datasets_uid <- c("nIHNMxuPUOR", "sBv1dj90IX6")
+  if ( Sys.info()["sysname"] == "Linux") {
+    ncores <- parallel::detectCores() - 1
+    doMC::registerDoMC( cores = ncores )
+    is_parallel <- TRUE
+  } else {
+    is_parallel <- FALSE
+  } 
+  vr_violations <- datimvalidation::validateData(vr_data,
+                                                 datasets = datasets_uid,
+                                                 parallel = is_parallel)
+  # rules_to_keep <- c(
+  #   "L76D9NGEPRS",
+  #   "rVVZmdG1KTb",
+  #   "zEOFo6X436M",
+  #   "oVtpQHVVeCV",
+  #   "r0CC6MQW5zc",
+  #   "vrS3kAtlJ4F",
+  #   "WB338HNucS7",
+  #   "tiagZGzSh6G",
+  #   "vkFHYHgfqCf",
+  #   "coODsuNsoXu",
+  #   "qOnTyseQXv8",
+  #   "Ry93Kc34Zwg",
+  #   "g0XwMGLB5XP",
+  #   "eb02xBNx7bD",
+  #   "SNzoIyNuanF"
+  # )
+  #
+  #vr_violations<-vr_violations[ vr_violations$id %in% rules_to_keep, ]
+  
+  diff <- gsub(" <= ", "/", vr_violations$formula)
+  
+  vr_violations$diff <-
+    sapply( diff, function(x) {
+      round( ( eval( parse( text = x) ) - 1) * 100, 2 )
+    })
+  
+  d$datim$vr_rules_check <- vr_violations %>% dplyr::filter(diff >= 5) %>%
+    dplyr::select(name,ou_name,mech_code,formula,diff) 
+
+ d
+  
+}
+
+
+adornSiteData<-function(d) {
+  
+  
+  cached_mechs <- "/srv/shiny-server/apps/datapack/mechs.rds"
+  
+  if (file.access(cached_mechs,4)) {
+    
+    mechs <-readRDS(cached_mechs)
+    
+  } else {
+    
+    mechs <- paste0(getOption("baseurl"), "api/sqlViews/fgUtV6e9YIX/data.csv") %>%
+      httr::GET() %>%
+      httr::content(., "text") %>%
+      readr::read_csv(col_names = TRUE) %>%
+      dplyr::select(mechanismCode = "code", partner, agency, ou)
+  }
+  
+
+  
+  site_list <- getSiteList(d$info$datapack_uid) %>%
+    dplyr::select(site_name=name,
+                  site_type,
+                  psnu,
+                  orgUnit=id)
+  
+  d$datim$site_data_pretty <- d$datim$site_data %>%
+    dplyr::mutate(
+      dataElement = datimvalidation::remapDEs(dataElement,"id","shortName"),
+      categoryOptionCombo = datimvalidation::remapCategoryOptionCombos(categoryOptionCombo,"id","shortName")
+      ) %>%
+    dplyr::left_join(site_list, by="orgUnit") %>%
+    dplyr::left_join( mechs, by = c(  "attributeOptionCombo" = "mechanismCode" )) %>% 
+    dplyr::select(-orgUnit)
+  
+  return(d)
+}
+  
+
+modalitySummaryChart <- function(df) {
+
+   df %>% 
+    dplyr::filter(!is.na(modality)) %>%
+    dplyr::group_by(modality, resultstatus) %>%
+    dplyr::summarise(value = sum(value)) %>%
+    dplyr::ungroup() %>%
+    dplyr::arrange(modality, desc(resultstatus)) %>% 
+    dplyr::mutate(resultstatus = factor(resultstatus, c("Negative", "Positive"))) %>%
+    ggplot(aes(
+      y = value,
+      x = reorder(modality, value, sum),
+      fill = resultstatus
+    )) +
+    geom_col() +
+    scale_y_continuous(labels = scales::comma) +
+    coord_flip() +
+    scale_fill_manual(values = c("#548dc0", "#59BFB3")) +
+    labs(y = "", x = "",
+         title = "COP19/FY20 Testing Targets",
+         subtitle = "modalities ordered by total tests") +
+    theme(legend.position = "bottom",
+          legend.title = element_blank(),
+          text = element_text(color = "#595959", size = 14),
+          plot.title = element_text(face = "bold"),
+          axis.ticks = element_blank(),
+          panel.background = element_blank(),
+          panel.grid.major.x = element_line(color = "#595959"),
+          panel.grid.minor.y = element_blank())
+
+}
