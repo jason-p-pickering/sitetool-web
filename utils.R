1d37d554be4c6b5c72d80189032c9e6f8af32a7b
Jason P. Pickering <jason.p.pickering@gmail.com>
2019-04-08 18:50:38 +0200

    Messages

--- utils.R
+++ utils.R
@@ -42,9 +42,9 @@ validateDataElementOrgunits<-function(d) {
     ) 
   
   if (inherits(de_check, "data.frame")) {
-    message<- paste0("ERROR! ",NROW(de_check),
+    message<- paste("ERROR! ",NROW(de_check),
       "invalid data element/orgunit associations found!")
-    flog.info(paste0(d$info$datapack_name, " SiteTool: ", message ), name="sitetool")
+    flog.info(paste(d$info$datapack_name, "SiteTool:", message ), name="sitetool")
     
     de_check %<>%
       dplyr::select(dataElement, orgUnit) %>%
@@ -90,7 +90,7 @@ validateDataElementDisaggs<-function(d){
                     categoryOptionCombo_name=datimvalidation::remapCategoryOptionCombos(categoryOptionCombo,mode_in = "id",mode_out = "shortName"))
     
     msg <- "ERROR!: Invalid data element / disagg combinations found!"
-    flog.info(paste0(d$info$datapack_name, " SiteTool: ", msg ), name="sitetool")
+    flog.info(paste(d$info$datapack_name, "SiteTool:", msg ), name="sitetool")
     d$info$warningMsg<-append(msg,d$info$warningMsg)
     d$info$had_error<-TRUE
   }
@@ -155,9 +155,9 @@ validateSiteData <- function(d) {
     dplyr::select(name,ou_name,mech_code,formula,diff) 
   
   if (NROW(d$datim$vr_rules_check) > 0 ) {
-    message<- paste0("ERROR! ",NROW(d$datim$vr_rules_check),
+    message<- paste("ERROR! ",NROW(d$datim$vr_rules_check),
                      "validation rule violations found. Check the Validation rules tab for details!")
-    flog.info(paste0(d$info$datapack_name, " SiteTool: ", message ), name="sitetool")
+    flog.info(paste(d$info$datapack_name, "SiteTool:", message ), name="sitetool")
     d$info$warningMsg<-append(message,d$info$warningMsg)
     d$info$had_error<-TRUE
   }
